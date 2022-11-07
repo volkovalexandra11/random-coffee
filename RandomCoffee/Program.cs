8636300d3b93878eabca54360775f9bad3ac6be7
Сергей Луговых <lugowyh.serezha@yandex.ru>
2022-11-07 22:25:10 +0500

    Listen for https on PORT env var or port from launchSettings.json

--- RandomCoffee/Program.cs
+++ RandomCoffee/Program.cs
@@ -4,19 +4,30 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-// builder.Services.AddEndpointsApiExplorer();
-// builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+if (!builder.Environment.IsDevelopment())
+{
+    var port = Environment.GetEnvironmentVariable("PORT");
+    if (port is null)
+        throw new Exception("Required PORT environment variable not found");
+    
+    builder.WebHost.UseUrls($"https://*:{port}");
+}
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-    // app.UseSwagger();
-    // app.UseSwaggerUI();
-// }
-
 app.UseHttpsRedirection();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
