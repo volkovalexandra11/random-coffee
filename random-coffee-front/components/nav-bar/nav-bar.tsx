4f3f8708321618929210a1040b64c53711890d59
Alexandra Volkova <alexandr.sazhev@yandex.ru>
2022-11-20 23:29:37 +0500

    Added button with icon

--- random-coffee-front/components/nav-bar/nav-bar.tsx
+++ random-coffee-front/components/nav-bar/nav-bar.tsx
@@ -1,30 +1,28 @@
-import style from './nav-bar.module.scss'
 import { FC } from 'react';
-import {TUser} from "../../types/user";
+import { NotificationBellIcon } from '@skbkontur/icons'
+import style from './nav-bar.module.scss'
+import { TUser } from "../../types/user";
+import { Button } from '@skbkontur/react-ui';
 
 
 export const NavBar: FC<TUser> = (props) => {
-    return (
-        <>
-            <div className={style.navBar}>
-                <div className={style.logo}>
-                    <a href={''}><h2>Cлучайный кофе</h2></a>
-                </div>
-                <div className={style.userPanel}>
-                    <button className={style.buttons}>
-                        <div className={style.test}>
-                            {/*<img src={'img/bell.png'} alt={''}/>*/}
-                        </div>
-                    </button>
-                    <a className={style.buttons} href={''}>
-                        <span className={style.text}>{props.firstName+" "+ props.lastName}</span>
-                    </a>
-                    <span className={style.line}/>
-                    <button className={style.exit} onClick={ () => alert('Logout!')}>
-                        <span className={style.text}>Выйти</span>
-                    </button>
-                </div>
-            </div>
-            <div style={{paddingTop: 50}}/>
-        </>)
-}
+	return (
+		<>
+			<div className={style.navBar}>
+				<div className={style.logo}>
+					<a href={''}><h2>Cлучайный кофе</h2></a>
+				</div>
+				<div className={style.userPanel}>
+					<Button className={style.buttons} icon={<NotificationBellIcon/>} borderless/>
+					<a className={style.buttons} href={''}>
+						<span className={style.text}>{props.firstName + " " + props.lastName}</span>
+					</a>
+					<span className={style.line}/>
+					<button className={style.exit} onClick={() => alert('Logout!')}>
+						<span className={style.text}>Выйти</span>
+					</button>
+				</div>
+			</div>
+			<div style={{ paddingTop: 50 }}/>
+		</>);
+};
