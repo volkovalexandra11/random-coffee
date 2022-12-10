c5d4a7bbd279dcbf3aced1df90af01e4324e2590
Alexandra Volkova <alexandr.sazhev@yandex.ru>
2022-12-10 19:23:56 +0500

    Get user data from Google

--- random-coffee-front/src/App.tsx
+++ random-coffee-front/src/App.tsx
@@ -1,29 +1,31 @@
 import React from 'react';
 import './App.scss';
-import {GroupPage} from "./pages/group-page";
-import { Route, Routes} from 'react-router-dom';
+import { GroupPage } from "./pages/group-page";
+import { Route, Routes } from 'react-router-dom';
 
 import { TGroup } from './types/group';
-import {mockGroupList} from "./mock/mock-group-list";
-import {NavBar} from "./components/nav-bar/nav-bar";
-import {CreateGroup} from "./pages/create-group";
+import { mockGroupList } from "./mock/mock-group-list";
+import { NavBar } from "./components/nav-bar/nav-bar";
+import { CreateGroup } from "./pages/create-group";
+import LoginWithGoogle from './pages/login-with-google';
 
 type Props = {
-  groupList: TGroup[];
+	groupList: TGroup[];
 }
 
 function App() {
-  let groupList = mockGroupList;
-  return (
-      <main>
-      <NavBar id={1} avatarPath={''} firstName={"Самсонов"} lastName={"Иван"}/>
-          <Routes>
-              <Route path={'/'} element={<GroupPage groupList={groupList}/>}/>
-              <Route path={'*'} element={<GroupPage groupList={groupList}/>}/>
-              <Route path={'/create'} element={<CreateGroup/>}/>
-          </Routes>
-      </main>
-  );
+	let groupList = mockGroupList;
+	return (
+		<main>
+			<NavBar id={1} avatarPath={''} firstName={"Самсонов"} lastName={"Иван"}/>
+			<Routes>
+				<Route path={'/'} element={<GroupPage groupList={groupList}/>}/>
+				<Route path={'/login'} element={<LoginWithGoogle/>}></Route>
+				<Route path={'/create'} element={<CreateGroup/>}/>
+				<Route path={'*'} element={<GroupPage groupList={groupList}/>}/>
+			</Routes>
+		</main>
+	);
 }
 
 export default App;
--- random-coffee-front/src/components/login-with-google/login-with-google.tsx
+++ random-coffee-front/src/components/login-with-google/login-with-google.tsx
@@ -1,9 +1,21 @@
 import { CredentialResponse, GoogleLogin } from '@react-oauth/google';
+import jwt_decode from 'jwt-decode';
 import style from './login-with-google.module.scss';
 
 export const LoginWithGoogleForm = () => {
+	const getUserInfo = async () => {
+		const a = await fetch('https://www.googleapis.com/oauth2/v1/userinfo?alt=json');
+		console.log(a);
+	}
+
 	const handleSuccess = (res: CredentialResponse) => {
 		console.log('Success', res);
+		if (res.credential) {
+			const decoded = jwt_decode(res.credential);
+			console.log(decoded);
+		}
+		else
+			handleError();
 	}
 
 	const handleError = () => {
