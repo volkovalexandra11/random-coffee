d2fd0c74dfed50f5907c0b220704a848a8e131c8
Alexandra Volkova <alexandr.sazhev@yandex.ru>
2023-01-10 17:25:59 +0500

    Deleted unnecessary markdown

--- random-coffee-front/src/components/login-with-google/login-with-google.tsx
+++ random-coffee-front/src/components/login-with-google/login-with-google.tsx
@@ -1,22 +1,10 @@
+import { Modal } from '@skbkontur/react-ui';
 import style from './login-with-google.module.scss';
-import { Button, Modal } from '@skbkontur/react-ui';
 import { GoogleSvg } from './google-svg';
-import { useNavigate } from "react-router-dom";
 
 export const LoginWithGoogleForm = () => {
-	const handleClick = () => {
-		async function loginOnBack() {
-			const resp = await fetch('/login', { method: 'POST' });
-			const respJson = await resp.json();
-			console.log(respJson);
-		}
-
-		loginOnBack();
-		// location.href='/login';
-		// useNavigate('/login');
-	}
 	return (
-		<Modal>Ы
+		<Modal>
 			<Modal.Header>Войдите или зарегистрируйтесь</Modal.Header>
 			<Modal.Footer>
 				<div className={style.center}>
